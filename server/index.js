86893f95e36d1e15c12a8ecdd4f447be62578dda
Xavier Olivares <xm.olivares@gmail.com>
2019-06-04 18:55:57 -0400

    added body parser

--- server/index.js
+++ server/index.js
@@ -2,9 +2,14 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const path = require('path')
+const bodyParser = require('body-parser')
 
 //logger middleware
 app.use(morgan('dev'));
 
 //serving up static assets from server with this middleware
 app.use(express.static(path.join(__dirname, './public')))
+
+//body parsers for possible requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
