3dc04fb45337f20e8ed5b3025b19cad086f0fe04
Xavier Olivares <xm.olivares@gmail.com>
2019-06-06 14:15:05 -0400

    changed social icon colors and font format

--- client/components/social.js
+++ client/components/social.js
@@ -5,10 +5,10 @@ export class Social extends Component {
     render () {
         return <div id='sociallinks'>
             {/* <Link className='wrap' to="/StackOverflow">StackOverflow</Link> */}
-            <SocialIcon className='wrap' target="_blank" url='https://www.linkedin.com/in/xavierolivaresb/' network='linkedin' />
-            <SocialIcon className='wrap' target="_blank" url='https://twitter.com/xavierolivaresb' network='twitter' />
-            <SocialIcon className='wrap' target="_blank" url='https://medium.com/@xavierolivares' network='medium' />
-            <SocialIcon className='wrap' target="_blank" url='https://github.com/xavierolivares' network='github' />
+            <SocialIcon className='wrap' target="_blank" url='https://www.linkedin.com/in/xavierolivaresb/' network='linkedin' bgColor='#000000' />
+            <SocialIcon className='wrap' target="_blank" url='https://twitter.com/xavierolivaresb' network='twitter' bgColor='#000000' />
+            <SocialIcon className='wrap' target="_blank" url='https://medium.com/@xavierolivares' network='medium' bgColor='#000000' />
+            <SocialIcon className='wrap' target="_blank" url='https://github.com/xavierolivares' network='github' bgColor='#000000' />
         </div>
     }
 
