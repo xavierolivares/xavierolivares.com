cc6ecc2c40d8e734df31eb7507607aa0ab751237
Xavier Olivares <xm.olivares@gmail.com>
2019-06-06 14:26:28 -0400

    tweaking around flexbox for home component

--- client/components/home.js
+++ client/components/home.js
@@ -1,10 +1,12 @@
 import React, {Component}
 from 'react'
+import {SocialIcon} from 'react-social-icons'
 
 export class Home extends Component {
     render () {
-        return <div>
+        return <div id='home'>
             <h1>work in progress</h1>
+            <SocialIcon url='http://xm.olivares@gmail.com' target='_blank' network='email' bgColor='#000000'/>
             </div>
     }
 }
