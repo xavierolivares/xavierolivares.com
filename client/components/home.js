adf6bc4982525e2193fc71739be5b23751f3bd54
Xavier Olivares <xm.olivares@gmail.com>
2019-06-07 09:58:54 -0400

    event handlers on home page, pending for hover changeable font

--- client/components/home.js
+++ client/components/home.js
@@ -3,9 +3,20 @@ from 'react'
 import {SocialIcon} from 'react-social-icons'
 
 export class Home extends Component {
+    constructor() {
+        super()
+        this.someHandler = this.someHandler.bind(this)
+    }
+    someHandler(event) {
+        console.log(event.target.value)
+    }
+    someOtherHandler(event) {
+        console.log(event.target.value)
+    }
+
     render () {
         return <div id='home'>
-            <h1>work in progress</h1>
+            <h1 onMouseEnter={this.someHandler} onMouseLeave={this.someOtherHandler}>work in progress</h1>
             {/* <SocialIcon url='http://xm.olivares@gmail.com' target='_blank' network='email' /> */}
             </div>
     }
