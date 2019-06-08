83fccda13bd41689be9859bac77946271fe3b4e9
Xavier Olivares <xm.olivares@gmail.com>
2019-06-07 22:25:17 -0400

    added media queries for mobile

--- client/components/about.js
+++ client/components/about.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
-import { Route } from "react-router-dom";
 
 export class About extends Component {
   render() {
