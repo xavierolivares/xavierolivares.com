2dd4b254c80b2fdfb48aa9079948b766d0abebe8
Xavier Olivares <xm.olivares@gmail.com>
2019-06-11 08:59:37 -0400

    added media query formatting

--- client/components/contact.js
+++ client/components/contact.js
@@ -5,7 +5,7 @@ export class Contact extends Component {
   render() {
     return (
       <div id="contact">
-        <h1>Email Me </h1>
+        <h1>Send an email</h1>
         {/* <p>Email Me</p> */}
         <SocialIcon
             target="_blank"
