2c7446a2a249900ed25222f3638a2b7f7836ab1c
Xavier Olivares <xm.olivares@gmail.com>
2019-06-10 20:58:36 -0400

    made another text change to contact component, someone help me

--- client/components/contact.js
+++ client/components/contact.js
@@ -5,8 +5,8 @@ export class Contact extends Component {
   render() {
     return (
       <div id="contact">
-        {/* <h1>Email me! </h1> */}
-        <p>Email Me</p>
+        <h1>Email Me </h1>
+        {/* <p>Email Me</p> */}
         <SocialIcon
             target="_blank"
             url="mailto:xm.olivares@gmail.com"
