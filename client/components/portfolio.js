517aeb209038784b4594e5bb910535889b293fb6
Xavier Olivares <xm.olivares@gmail.com>
2019-06-10 20:00:04 -0400

    added greenhousely project to work component

--- client/components/portfolio.js
+++ client/components/portfolio.js
@@ -4,7 +4,9 @@ export class Work extends Component {
   render() {
     return (
       <div id="work">
-        <h1>work in progress</h1>
+        {/* <h1>work in progress</h1> */}
+        <img src={'https://cdn.shopify.com/s/files/1/1814/7347/users/32x32_Xavierolivares.jpg?v=1500598389'} />
+        <a target="_blank" href='https://www.greenhousely.com'>Visit Greenhousely!</a>
       </div>
     );
   }
