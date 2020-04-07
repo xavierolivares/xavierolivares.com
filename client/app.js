e9b4252f884ec47b91c909f2bad7ec893cbfd0d6
Xavier Olivares <xm.olivares@gmail.com>
2020-04-07 19:12:36 -0400

    updated work history

--- client/app.js
+++ client/app.js
@@ -36,7 +36,7 @@ export class App extends Component {
               <div className='col-narrow'>
               {/* <!-- Job Details: copy this whole block to add more jobs --> */}
               <h3>Software Engineer</h3>
-              <p>BounceX soon to be Wunderkind</p>
+              <p>BounceX (soon to be Wunderkind)</p>
               <p>09/2019 - 04/2020</p>
           </div> 
               {/* <!-- Add as many paragraphs as you need. -->
@@ -65,9 +65,9 @@ export class App extends Component {
               {/* <!-- This list can be deleted if you don't want to use it. --> */}
               <p>Summary:</p>
               <ul>
-                  <li>Providing online competitive prices for greenhouses using a drop-shipping business model.</li>
-                  <li>Built with Shopify. Setup business and tax credentials for sale of goods within the contiguous U.S.</li>
-                  <li>Illustrates a start-to-finish approach for a seasonal business containing a complete eCommerce workflow.</li>
+                <li>Provided online competitive prices for greenhouses using a drop-shipping business model.</li>
+                <li>Fully operational Shopify business made for the sale of goods within the United States.</li>
+                <li>Contained a complete eCommerce workflow including: Niche Selection, Website Build, Domain Acquisition, Marketing, Paid Advertising, Email Service Provider Integration, Business and Tax Preparation, Dropship Contracting (Remote Warehouse Fulfillment), and eCommerce Checkout</li>
               </ul>
               {/* <!-- end of Job Details block. --> */}
           </div>   
@@ -101,15 +101,11 @@ export class App extends Component {
                   {/* <!-- This list can be deleted if you don't want to use it. --> */}
                   <p>Summary:</p>
                   <ul>
-                          <li>Tested functionality over three website redesigns to help ensure a positive consumer experience.</li>
-                          <li>Continuously tested and shared logs with development teams: Salesforce, Apprise/Aptean, and Blue Acorn.</li>
-                          <li>Played a significant role in implementing Zendesk for the newest iteration of the website.</li>
-                          <li>Improved customer service satisfaction by 7-8% and developed new hire training protocols.</li>
-                          <li>Reduced chargeback rate by 95% with a self-developed risk protocol prior to implementing Kount software.</li>
+                          <li>Tested functionality over three website redesigns to help ensure a positive UX/UI experience. Associated teams were Salesforce, Apprise/Aptean, and Blue Acorn.</li>
+                          <li>Played a significant role in implementing Zendesk for the newest iteration of the website, improving customer service satisfaction by 7-8%.</li>
                   </ul>
                   {/* <!-- end of Job Details block. --> */}
               </div>
-
           </div>
       </section>
 
@@ -121,16 +117,13 @@ export class App extends Component {
               {/* <!-- School details: copy this whole block to add more schools. --> */}
               <h3>Fullstack Academy of Code - New York, NY</h3>
               <p>Certificate of Completion: React, Redux, Node, Express, Sequelize, Postgres</p>
-              
               {/* <!-- Add as many paragraphs as you need. --> */}
-              <p>Fullstack Academy is an immersive software engineering coding bootcamp located in New York City and Chicago. Students of the full-time flagship course learn full stack JavaScript over the course of a 13-week, on-campus program</p>
               {/* <!-- End of school details. --> */}
+
               {/* <!-- School details: copy this whole block to add more schools. --> */}
               <h3>Muhlenberg College - Allentown, PA</h3>
               <p>Bachelor of Arts in Business Administration, Concentration: Entrepreneurship</p>
-              
               {/* <!-- Add as many paragraphs as you need. --> */}
-              <p>Muhlenberg College is a private liberal arts college in Allentown, Pennsylvania. Founded in 1848, Muhlenberg is affiliated with the Evangelical Lutheran Church in America and is named for Henry Melchior Muhlenberg, the German patriarch of the Lutheran Church in America.</p>
               {/* <!-- End of school details. --> */}
           </div>
       </section>
